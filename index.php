0cfc518447b2a0f4fdce7d1c204e0d8813b281f0
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-12-20 22:17:34 -0300

    Estrutura do chat

--- index.php
+++ index.php
@@ -12,9 +12,19 @@
     <div id="conteudo">
         <div id="caixa-chat">
             <div id="chat">
-
+                <div id="dados-chat">
+                    <span>Grazziano: </span>
+                    <span>Texto da Mensagem: </span>
+                    <span>Hora: </span>
+                </div>
             </div>
         </div>
+
+        <form action="index.php" method="post">
+            <input type="text" name="nome" id="" placeholder="Preencha o seu nome">
+            <textarea name="mensagem" id="" cols="30" rows="10" placeholder="Insira uma mensagem"></textarea>
+            <input type="submit" name="enviar" value="Enviar">
+        </form>
     </div>
 
 </body>
